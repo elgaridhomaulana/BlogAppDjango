32d81795fb3cb94adf26dc9098555819ee1391aa
elgaridhomaulana <elgaridhomaulana@gmail.com>
2019-07-14 15:49:57 +0700

    creating CRUD function in blog app

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -11,3 +12,7 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('post_detail', args=[str(self.id)])
+		# return reverse('home')
--- blog/tests.py
+++ blog/tests.py
@@ -42,3 +42,22 @@ class BlogTest(TestCase):
 		self.assertEqual(no_response.status_code, 404)
 		self.assertContains(response, 'This is title of your post')
 		self.assertTemplateUsed(response, 'post_detail.html')
+
+	def test_post_create_view(self):
+		response = self.client.get(reverse('post_new'), {
+			'title': 'New title',
+			'body': 'New text',
+			'author': self.user,
+			})
+		self.assertEqual(response.status_code, 200)
+
+	def test_post_update_view(self):
+		response = self.client.post(reverse('post_edit', args='1'), {
+			'title': 'Updated title',
+			'body': 'Updated text',
+			})
+		self.assertEqual(response.status_code, 302)
+
+	def test_post_delete_view(self):
+		response = self.client.get(reverse('post_delete', args='1'))
+		self.assertEqual(response.status_code, 200)
